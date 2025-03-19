e826c5a05b8079e96d86e023f3c8206c37a31206
Sergio Castro <checo-.911@hotmail.com>
2025-03-18 18:24:24 -0600

    challengue entrega

--- challenge-amigo-secreto_esp-main/challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,49 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaDeNombres = []
+
+
+
+function sortearAmigo(lista) {
+  numeroMaximo =  lista.length -1
+  numeroAleatorio = parseInt(Math.random()*numeroMaximo)+1
+  amigoSorteado = lista[numeroAleatorio]
+  asignarTexto("#resultado", `el amigo sorteado es ${amigoSorteado}`)
+  listaDeNombres = []
+  
+    
+}
+
+function asignarTexto(elemento, texto) {
+    elementoHTML = document.querySelector(elemento)
+    elementoHTML.innerHTML = texto 
+    return
+    
+}
+
+
+function limpiarCuadro() {
+    document.querySelector("#amigo").value =""
+    
+    
+}
+
+function agregarAmigo(lista) {
+    
+
+   let nombreAgregado = document.getElementById("amigo").value 
+
+    if(nombreAgregado === ""){
+        alert("agrega un valor valido")
+
+    }else{
+     lista.push(nombreAgregado)
+     nombreAgregado = asignarTexto("#listaAmigos", nombreAgregado)
+     console.log(listaDeNombres)
+     limpiarCuadro()
+     return  }
+  
+    
+}
+
+
+
